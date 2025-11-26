73cdc830a86c7394b4b21d81e2b1cf5a4511654b
Adam <a.r.evans@outlook.com>
2025-11-26 09:15:02 +0000

    Update background image in Podium component to use a GIF for enhanced visual appeal

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Podium = ({ brokers }) => {
 
   return (
     <div className="min-h-screen bg-white flex flex-col" style={{
-      backgroundImage: `url(${ceremonyImage})`,
+      backgroundImage: `url('https://c.tenor.com/BzTSwWeFTXkAAAAC/tenor.gif')`,
       backgroundSize: '100% 125%',
       backgroundPosition: '-70% 0%',
       backgroundRepeat: 'no-repeat',
